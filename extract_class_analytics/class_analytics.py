7c17b18661640558fb8bb1aa70e10075558edbee
ekayanful <ekayanful98@gmail.com>
2024-03-04 13:54:05 +0000

    modifications made

--- extract_class_analytics/class_analytics.py
+++ extract_class_analytics/class_analytics.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import sys
 
+def extract_class_list(insendi_file, class_name):
+    class_list = pd.read_csv(insendi_file)
+    extract = class_list.loc[class_list['group_name'] == class_name, ['given_name', 'family_name']]
+    class_list = extract[['given_name', 'family_name']]
+    class_list['Students'] = extract['given_name'] + " " + extract['family_name']
+    class_list.drop(['given_name', 'family_name'], axis=1, inplace=True)
+    temp = '_'.join(class_name.split(' '))
+    class_list.to_csv(f"./class_lists/{temp}_list.csv", index=False)
+    return class_list
+        
 def load_data(insendi_file, class_list):
     analytics = pd.read_csv(insendi_file)
     file_extension = class_list.split('.')[-1]
     
     if file_extension == 'txt' or file_extension == 'csv':
-        classList = pd.read_csv(f"{class_list}", header=None, names=['Student'])
+        classList = pd.read_csv(f"{class_list}", header=None, names=['Students'])
     elif file_extension == 'xlsx':
-        classList = pd.read_excel(f"{class_list}", header=None, names=['Student'])
+        classList = pd.read_excel(f"{class_list}", header=None, names=['Students'])
     else:
         sys.exit("Class list file format must be txt, csv, or xlsx")
     return analytics, classList
@@ -43,8 +53,25 @@ def extract_sections(analytics):
 
 def main():
     if len(sys.argv) != 3:
-        sys.exit("Usage: python analytics.py [insendi_file] [class_list]")
-    insendi_file, class_list = sys.argv[1], sys.argv[2]
+        sys.exit("Usage: python [analytics.py] [insendi csv file] [class_name]")
+    
+    user = str(input("Do you have a class list? (y/n): \n"))
+    if user == 'y':
+        insendi_file = sys.argv[1]
+        temp = sys.argv[2]
+        class_list = f"./class_lists/{temp}_list.csv"
+        
+    elif user == 'n':
+        insendi_file_list = str(input("Enter insendi list csv file name without .csv: \n"))
+        class_ = sys.argv[2]#str(input("Enter class list name (AWB Beta Class or HENKEL Gamma Class etc): \n"))
+        class_name = " ".join(class_.split('_'))
+        extract_class_list(f"./class_lists/{insendi_file_list}.csv", class_name)
+        temp = '_'.join(class_name.split(' '))
+        class_list = f"./class_lists/{temp}_list.csv"
+        insendi_file = sys.argv[1]
+    else:
+        sys.exit("Input must be y or n")
+    
     print("Loading data...\n")
     analytics, classList = load_data(insendi_file, class_list)
     print("Data loaded.\n")
@@ -54,8 +81,8 @@ def main():
     print("Section analytics extracted.\n")
         
     print("Extracting class analytics...\n")
-    merged = pd.merge(analytics, classList, on='Student', how='inner')
-    merged[columns_extract].to_csv('class_analytics.csv', index=False)
+    merged = pd.merge(analytics, classList, left_on='Student', right_on='Students', how='inner')
+    merged[columns_extract].to_csv('./analytics/class_analytics.csv', index=False)
     print("Class analytics extracted.\n")
     
 if __name__ == "__main__":
